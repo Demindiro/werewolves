961614b2b3d67947f6529bef9ea590d4e4adb0d6
David Hoppenbrouwers <david@salt-inc.org>
2020-10-21 01:44:10 +0200

    Lock database when updating a game's state

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import random
 import sys
 from flask import Flask, redirect, url_for, render_template, request, session, Blueprint
 from game import Game
+import gamedb
 
 
 def eprint(*args, **kwargs):
@@ -11,9 +12,6 @@ def eprint(*args, **kwargs):
 
 
 api = Blueprint('api', __name__)
-games = {}
-total_games = 0
-
 
 
 @api.errorhandler(405)
@@ -32,20 +30,20 @@ def create_game():
     if name is None:
         return {'message': f'Name field is not found or empty'}, 400
     code = None
-    while code is None or code in games:
+    while code is None or gamedb.load_game(code) is not None:
         code = ''.join((random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(6)))
-    game = Game()
-    games[code] = game
-    global total_games
-    total_games += 1
-    game.add_player(name)
+    with gamedb.lock:
+        gamedb.create_game(code)
+        game = gamedb.load_game(code)
+        game.add_player(name)
+        gamedb.save_game(code, game)
     session[code] = name
     return {'code': code}, 201
 
 
 @api.route('/info/<string:code>/')
 def get_game_info(code: str):
-    game = games.get(code)
+    game = gamedb.load_game(code)
     if game is None:
         return {'message': f'Game {code} not found'}, 404
     return {
@@ -58,19 +56,21 @@ def get_game_info(code: str):
 
 @api.route('/action/<string:code>/<string:activity>/', methods=['POST'])
 def perform_action(code: str, activity: str):
-    game = games.get(code)
-    if game is None:
-        return {'message': f'Game {code} not found'}, 404
-    name = session.get(code)
-    if name is None:
-        return {'message': f'Name is not set'}, 404
-    game.perform_action(name, activity, request.form)
+    with gamedb.lock:
+        game = gamedb.load_game(code)
+        if game is None:
+            return {'message': f'Game {code} not found'}, 404
+        name = session.get(code)
+        if name is None:
+            return {'message': f'Name is not set'}, 404
+        game.perform_action(name, activity, request.form)
+        gamedb.save_game(code, game)
     return {}, 200
 
 
 @api.route('/info/<string:code>/<string:activity>/')
 def get_game_info_activity(code: str, activity: str):
-    game = games.get(code)
+    game = gamedb.load_game(code)
     if game is None:
         return {'message': f'Game {code} not found'}, 404
     name = session.get(code)
@@ -84,30 +84,33 @@ def join_game(code: str):
     name = request.form.get('name')
     if name is None:
         return {'message': f'Name field is not found or empty'}, 400
-    game = games.get(code)
-    if game is None:
-        return {'message': f'Game {code} not found'}, 404
-    if name in game.player_roles:
-        if code in session:
-            return {'message': f'Already joined game {code}'}, 400
-        else:
-            return {'message': f'Name {name} already taken'}, 403
-    if game.started:
-        return {'message': f'Game {code} has already started'}, 404
-    if __debug__:
+    with gamedb.lock:
+        game = gamedb.load_game(code)
+        if game is None:
+            return {'message': f'Game {code} not found'}, 404
+        if name in game.player_roles:
+            if code in session:
+                return {'message': f'Already joined game {code}'}, 400
+            else:
+                return {'message': f'Name {name} already taken'}, 403
+        if game.started:
+            return {'message': f'Game {code} has already started'}, 404
         if code in session:
             eprint(f'Player {name} has code {code} set but has not joined the game')
-    game.add_player(name)
+        game.add_player(name)
+        game = gamedb.save_game(code, game)
     session[code] = name
     return {}, 200
 
 
 @api.route('/start/<string:code>/', methods=['POST'])
 def start_game(code: str):
-    game = games.get(code)
-    if game is None:
-        return {'message': f'Game {code} not found'}, 404
-    if game.started:
-        return {'message': f'Game {code} has already started'}, 404
-    game.start()
-    return {}
+    with gamedb.lock:
+        game = gamedb.load_game(code)
+        if game is None:
+            return {'message': f'Game {code} not found'}, 404
+        if game.started:
+            return {'message': f'Game {code} has already started'}, 404
+        game.start()
+        gamedb.save_game(code, game)
+    return {}, 200
